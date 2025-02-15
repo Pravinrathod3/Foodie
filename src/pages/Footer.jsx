abea1b0d970c80881ad975457771576468c3d031
Pravin Rathod <pravinrathod4532@gmail.com>
2025-02-15 12:33:06 +0530

    deployment error

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import picture from '../assets/fotimage.png'
-import logo from '../assets/Logo.png'
+import logo from '../assets/logo.png'
 
 function Footer() {
   return (
